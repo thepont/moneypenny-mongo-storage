d1daf5efaf262dbc3b1a9da3adef3f289d60c0db
Paul Esson <paul.esson@blueflag.com.au>
2015-11-12 09:54:24 +1100

    try older version of node

--- Gruntfile.js
+++ Gruntfile.js
@@ -47,10 +47,28 @@ module.exports = function(grunt){
                     // reportFormats:  ['lcov', 'html'] 
                 }
             }
-        }
+        },
+        coveralls: {
+        // Options relevant to all targets
+            options: {
+              // When true, grunt-coveralls will only print a warning rather than
+              // an error, to prevent CI builds from failing unnecessarily (e.g. if
+              // coveralls.io is down). Optional, defaults to false.
+              force: false
+            },
+
+            post_lcov: {
+            // LCOV coverage file (can be string, glob or array)
+                src: 'coverage/lcov.info',
+                options: {
+                    // Any options for just this target
+                }
+            },
+        },
     });
     grunt.loadNpmTasks('grunt-mocha-istanbul');
-    grunt.registerTask('test', ['mocha_istanbul:coverage']);
+    grunt.loadNpmTasks('grunt-coveralls');
+    grunt.registerTask('test', ['mocha_istanbul:coverage', 'coveralls:post_lcov']);
 };
 
 
