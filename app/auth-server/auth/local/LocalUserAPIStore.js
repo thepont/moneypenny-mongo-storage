45e06d56274761386049ffb599b9ed0804dbf22f
Paul Esson <paul.esson@blueflag.com.au>
2015-11-12 14:22:48 +1100

    Removed some unused code and added tests

--- app/auth-server/auth/local/LocalUserAPIStore.js
+++ app/auth-server/auth/local/LocalUserAPIStore.js
@@ -10,6 +10,7 @@ module.exports = {
      *@param {string} username username to search for.
      *@returns <Promise{User}> User that has the corresponding username
      */ 
+    
     getUser: function(username){
         var query = LocalUserQuery().usernameEqualsIgnoreCase(username);
         return localUsersCollection.findOne(query);                   
@@ -21,21 +22,21 @@ module.exports = {
      * @param {string} password password to authenticate with.
      */ 
     
-    verifyUser: function(username, password){
-        var query = LocalUserQuery().usernameEqualsIgnoreCase(username).showHash();
-        localUsersCollection.find(query).then(function(user){
-            return PasswordCrypto.checkPassword(password, user.hash).then(() => {
-                return LocalUserQuery.findByUsername(username);
-            });
-        });
-    },
+    // verifyUser: function(username, password){
+    //     var query = LocalUserQuery().usernameEqualsIgnoreCase(username).showHash();
+    //     return localUsersCollection.findOne(query).then(function(user){
+    //         return PasswordCrypto.checkPassword(password, user.hash).then(() => {
+    //             return LocalUserQuery.findByUsername(username);
+    //         });
+    //     });
+    // },
 
     /**		
      * Returns the password hash for a user.
      * @param {string} username username of the user to find the hash of
      * @returns {Promise<string>} String containging the hash of the users password.
      */
-
+    
     getUserHash: function(username){
         var query = LocalUserQuery().usernameEqualsIgnoreCase(username).showHash();
         return localUsersCollection.findOne(query);
@@ -47,9 +48,9 @@ module.exports = {
      * @param {string} username to add to the system.
      * @param {string} password password to assign to the new user.
      */ 
-    addUser: function(user, username, password){
+    // addUser: function(user, username, password){
         
-    },
+    // },
     
     /**
      * Changes a users password.
@@ -57,14 +58,15 @@ module.exports = {
      * @param {string} username username of the user that requires the password change. 
      * @param {string} newPassword password to change the password to.
      */
-    changePassword: function(username, password){
+    // changePassword: function(username, password){
 
-    },
+    // },
+    
     /**
      *List all local users in the system.
      *@returns {Promise<User[]>} promse with list of all the local users in the system.
      */ 
-    listUsers: function(){
+    // listUsers: function(){
         
-    }
+    // }
 }
