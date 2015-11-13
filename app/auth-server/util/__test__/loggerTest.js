ad3c71b1868b9763d2fcefa54b2ea7b2ceb44f22
Paul Esson <paul.esson@blueflag.com.au>
2015-11-13 18:14:21 +1100

    Logger tests

---
+++ app/auth-server/util/__test__/loggerTest.js
@@ -0,0 +1,11 @@
+var proxyquire = require('proxyquire');
+var sinon = require('sinon');
+describe('SamlStrategy', () => {
+	it('Exposes the elephas logger', () =>{
+		var fakeLogger = {name: 'fakeLogger'};
+		var logger = proxyquire('../logger', {
+			'elephas/lib/logger' : fakeLogger
+		});
+		logger.should.equal(fakeLogger)
+	});
+});
