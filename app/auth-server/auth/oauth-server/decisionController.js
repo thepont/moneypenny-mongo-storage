b22cf09532d374fb10c533b2313b8cfcb9f2e0df
Paul Esson <paul.esson@blueflag.com.au>
2015-11-13 11:03:10 +1100

    Test for oAuth2Server.js

--- app/auth-server/auth/oauth-server/decisionController.js
+++
@@ -1,9 +0,0 @@
-var authorization = require('oauth20-provider/lib/authorization');
-/**
- * Don't show authorization page just allow client to authorize
- */ 
-module.exports = function(req, res, client, scope, user){
-    req.body.decision = 1;
-    req.method = POST;
-    return authorization(req, res);
-};
