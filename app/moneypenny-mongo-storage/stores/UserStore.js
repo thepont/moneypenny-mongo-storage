b16fd2ee1016fe6338b199395286e91879d9d002
Paul Esson <paul.esson@blueflag.com.au>
2016-01-14 10:14:50 +1100

    Revert "require path fixes"
    This reverts commit 5dc03f5f6459cd7e04973f9f927225f8e52ac0d8.

--- app/moneypenny-mongo-storage/stores/UserStore.js
+++ app/moneypenny-mongo-storage/stores/UserStore.js
@@ -1,4 +1,4 @@
-var Collection = require('../db/collection');
+var Collection = require('moneypenny-mongo-storage/db/collection');
 var UserQuery = require('./UserQuery');
 
 module.exports = function(collection){
@@ -8,17 +8,17 @@ module.exports = function(collection){
         * Saves a user in the database.
         * @param {SessionUser} sessionUser - user to save into the database
         * @return {Promise<ObjectId>} - id of the user object in the database
-        */
-
+        */ 
+        
         save: function(user){
-            return userCollection.save(user);
+            return userCollection.save(user); 
         },
-
+        
         /**
         * Loads a session users from the database
         * @param {ObjectId} id - id of the user to load.
         * @returns {Promise<SessionUser>} - SessionUser loaded from id.
-        */
+        */ 
         fetchById: function(id){
             var query = UserQuery().idEquals(id);
             return userCollection.findOne(query);
